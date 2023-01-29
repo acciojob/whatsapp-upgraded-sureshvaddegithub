92dd38b51cd318e9c7df3583aac1dfd2f3df0714
vadde suresh <sureshvadde1076@gmail.com>
2023-01-29 07:15:52 +0530

    message

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -10,6 +10,12 @@ public class Message {
     public Message() {
     }
 
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
     public int getId() {
         return id;
     }
